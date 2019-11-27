1c6cc17bd4b24bd3c6101f6a26f9c001bb437de6
christophe <christophe.darmayan@outlook.fr>
2019-11-27 14:41:28 +0100

    version 1.1 rajout de routes et de controllers

---
+++ src/Controller/LibelluleController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class LibelluleController{
+
+
+public function voler(){
+
+    return new Response("La libéllule vient de s'envoler !!");
+}
+
+
+
+
+
+}
--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,16 +2,34 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+ use Symfony\Component\HttpFoundation\Response;
 
+use Symfony\Component\Routing\Annotation\Route;
 class LuckyController
 {
+    /*
+    *@Route("lucky/number")
+    */
     public function number()
     {
+        
         $number = random_int(0, 100);
 
         return new Response(
             '<html><body>Lucky number: '.$number.'</body></html>'
         );
     }
+    public function accueil($slug)
+    {
+        return new Response(sprintf('<html><body> BIENVENUE DANS LA NATURE % s !!</body></html>',$slug)
+            
+        );
+    }
+    public function bienvenu()
+    {
+        return new Response('<html><body> BIENVENUE DANS LA NATURE !!</body></html>'
+            
+        );
+    }
+    
 }
